0b4a3775d4fd2d4fbe05d4964af8cb286689b604
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2024-05-22 16:23:40 +0900

    feat: Naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [name, setName] = useState("TS");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -15,18 +14,12 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <table>
+      {[...Array(6)].map((_, i) => {
+          const item = [...Array(6)].map((_, i) => i).map(y => <td>{y}</td>);
+          return <tr>{item}</tr>
+        })}
+      </table>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
